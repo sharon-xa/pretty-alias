6e3e336d752ddf091d7736956866f5a5e4f92081
sharon <ali93456@gmail.com>
2024-08-29 14:27:44 +0300

    add the ability to log stuff to a file
    logging wasn't possible cuz this is a TUI program, so we have to log stuff to a file.
    this won't happen during production, only in the developer environment.

--- main.go
+++ main.go
@@ -2,13 +2,33 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"log"
 	"os"
 
 	tea "github.com/charmbracelet/bubbletea"
+	"github.com/joho/godotenv"
 )
 
 func main() {
-	p := tea.NewProgram(model{}, tea.WithAltScreen())
+	godotenv.Load(".env")
+
+	if os.Getenv("PRETTY_ALIAS_DEBUG") == "" {
+		log.SetOutput(io.Discard) // Disable logging by default
+	} else {
+
+		logFile, err := os.OpenFile("debug.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
+		if err != nil {
+			log.Fatalln("Failed to open log file:", err)
+		}
+
+		log.SetOutput(logFile)
+		defer logFile.Close()
+
+		log.Println("Program started")
+	}
+
+	p := tea.NewProgram(&model{}, tea.WithAltScreen())
 	if _, err := p.Run(); err != nil {
 		fmt.Fprintf(os.Stderr, "Error: %v\n", err)
 		os.Exit(1)
