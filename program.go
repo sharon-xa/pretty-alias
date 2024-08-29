3658584d076b3bb6d32a4b410488a9b3e63b64ac
sharon <ali93456@gmail.com>
2024-08-29 14:37:21 +0300

    fixes: the TUI application was slow and buggy
    1. save the aliases so that you don't have to refetch them with every update
    2. we can receive the aliases message and render the table before receving the window size message which will result in a fixed width set by the table, this is fixed now
    3. log some stuff for debugging

--- program.go
+++ program.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"log"
 	"strings"
 
 	tea "github.com/charmbracelet/bubbletea"
 	"github.com/charmbracelet/lipgloss"
+
+	"github.com/sharon-xa/pretty-alias/system"
+	"github.com/sharon-xa/pretty-alias/table"
 )
 
 type model struct {
+	aliases      []string
 	table        string
 	err          error
 	width        int
@@ -16,36 +21,55 @@ type model struct {
 	scrollOffset int
 }
 
-func printTable(width int, height int) tea.Cmd {
-	return func() tea.Msg {
-		aliases, err := getAliases()
-		if err != nil {
-			return errMsg{err}
-		}
-		rows := getTableRows(aliases)
-		t := getTable(rows, width, height)
-		return tableMsg(t.String())
-	}
-}
-
 type (
-	tableMsg string
-	errMsg   struct{ err error }
+	aliasesMsg []string
+	tableMsg   string
+	errMsg     struct{ err error }
 )
 
 func (e errMsg) Error() string { return e.err.Error() }
 
-func (m model) Init() tea.Cmd {
-	return nil
+func fetchAliases() tea.Cmd {
+	return func() tea.Msg {
+		aliases, err := system.GetAliases()
+		if err != nil {
+			log.Println("couldn't get the aliases\nError:", err)
+			return errMsg{err}
+		}
+		return aliasesMsg(aliases)
+	}
 }
 
-func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+func renderTable(aliases []string, width int, height int) tea.Cmd {
+	return func() tea.Msg {
+		rows := table.GetTableRows(aliases)
+		log.Println("WIDTH:", width, "HEIGHT:", height)
+		t := table.GetTable(rows, width, height)
+		return tableMsg(t.String())
+	}
+}
+
+func (m *model) Init() tea.Cmd {
+	if m.ready {
+		log.Println("Init called again, but model is already initialized")
+		return nil
+	}
+
+	log.Println("Init called")
+	return tea.Batch(fetchAliases())
+}
+
+func (m *model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	switch msg := msg.(type) {
-	case tea.WindowSizeMsg:
-		m.width = msg.Width
-		m.height = msg.Height
-		m.ready = true
-		return m, printTable(m.width, m.height)
+	case aliasesMsg:
+		// If the window size is not yet set, store aliases and wait for WindowSizeMsg
+		m.aliases = msg
+		if m.width == 0 || m.height == 0 {
+			log.Println("Received aliasesMsg but window size not set, waiting...")
+			return m, nil
+		}
+		log.Println("AliasesMsg Update, Width:", m.width, ", Height:", m.height)
+		return m, renderTable(m.aliases, m.width, m.height)
 
 	case tableMsg:
 		m.table = string(msg)
@@ -55,15 +79,21 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.err = msg
 		return m, tea.Quit
 
+	case tea.WindowSizeMsg:
+		log.Println("WindowSizeMsg Update, Width:", m.width, ", Height:", m.height)
+		m.width = msg.Width
+		m.height = msg.Height
+		m.ready = true
+		return m, renderTable(m.aliases, m.width, m.height)
+
 	case tea.KeyMsg:
 		switch msg.String() {
-		case "q":
-			return m, tea.Quit
-		case "ctrl+c":
+		case "q", "ctrl+c":
 			return m, tea.Quit
 		case "up":
 			if m.scrollOffset > 0 {
 				m.scrollOffset--
+				return m, nil
 			}
 		case "down":
 			lines := strings.Split(m.table, "\n")
@@ -71,6 +101,7 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 				maxOffset := len(lines) - m.height
 				if m.scrollOffset < maxOffset {
 					m.scrollOffset++
+					return m, nil
 				}
 			}
 		}
@@ -78,8 +109,9 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 	return m, nil
 }
 
-func (m model) View() string {
+func (m *model) View() string {
 	if m.err != nil {
+		log.Println("Error encountered:", m.err)
 		return m.err.Error()
 	}
 
@@ -87,9 +119,7 @@ func (m model) View() string {
 		return "Waiting for window size..."
 	}
 
-	tableStr := m.table
-	lines := strings.Split(tableStr, "\n")
-
+	lines := strings.Split(m.table, "\n")
 	start := m.scrollOffset
 	end := m.scrollOffset + m.height
 	if end > len(lines) {
